bbcc780cfd968d348c0166ae58e0b84ff3310ff0
gopal <gopal@splashbox.com.au>
2018-11-02 01:38:44 +0000

    changes.

--- src/Extensions/PageSeoExtension.php
+++ src/Extensions/PageSeoExtension.php
@@ -84,13 +84,13 @@ class PageSeoExtension extends DataExtension
         parent::updateCMSFields($fields);
 
         $fields->addFieldsToTab('Root.Main', [
-            ToggleCompositeField::create(null, 'Facebook SEO', [
+            ToggleCompositeField::create('FacebookSeoComposite', 'Facebook SEO', [
                 DropdownField::create('FacebookPageType', 'Type', FacebookMetaGenerator::getValidTypes()),
                 TextField::create('FacebookPageTitle', 'Title')->setAttribute('placeholder', $this->getOwner()->Title)->setRightTitle('If blank, inherits default page title')->setTargetLength(45, 25, 70),
                 UploadField::create('FacebookPageImage', 'Image')->setRightTitle('Facebook recommends images to be 1200 x 630 pixels. If no image is provided, facebook will choose the first image that appears on the page which usually has bad results')->setFolderName('seo'),
                 TextareaField::create('FacebookPageDescription', 'Description')->setAttribute('placeholder', $this->getOwner()->MetaDescription ?: $this->getOwner()->dbObject('Content')->LimitCharacters(297))->setRightTitle('If blank, inherits meta description if it exists or gets the first 297 characters from content')->setTargetLength(200, 160, 320),
             ]),
-            ToggleCompositeField::create(null, 'Twitter SEO', [
+            ToggleCompositeField::create('TwitterSeoComposite', 'Twitter SEO', [
                 TextField::create('TwitterPageTitle', 'Title')->setAttribute('placeholder', $this->getOwner()->Title)->setRightTitle('If blank, inherits default page title')->setTargetLength(45, 25, 70),
                 UploadField::create('TwitterPageImage', 'Image')->setRightTitle('Must be at least 280x150 pixels')->setFolderName('seo'),
                 TextareaField::create('TwitterPageDescription', 'Description')->setAttribute('placeholder', $this->getOwner()->MetaDescription ?: $this->getOwner()->dbObject('Content')->LimitCharacters(297))->setRightTitle('If blank, inherits meta description if it exists or gets the first 297 characters from content')->setTargetLength(200, 160, 320),
