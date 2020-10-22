d07f72b4817a3dbc94cde63e2294f421bbb8afc1
Phil Quinn <phil@quinn.com>
2020-10-22 09:32:02 -0700

    only update fields once

--- src/Extensions/PageHealthExtension.php
+++ src/Extensions/PageHealthExtension.php
@@ -95,25 +95,26 @@ class PageHealthExtension extends DataExtension
      */
     public function updateCMSFields(FieldList $fields)
     {
-        parent::updateCMSFields($fields);
-        if (Controller::curr() instanceof HistoryViewerController) { // avoid breaking the history comparison UI
+        if (!$fields->dataFieldByName('FocusKeyword')) {
+            if (Controller::curr() instanceof HistoryViewerController) { // avoid breaking the history comparison UI
             return;
-        }
-        if ($this->owner instanceof \SilverStripe\ErrorPage\ErrorPage) {
-            return;
-        }
+            }
+            if ($this->owner instanceof \SilverStripe\ErrorPage\ErrorPage) {
+                return;
+            }
 
-        $fields->addFieldsToTab('Root.Main', [
-            ToggleCompositeField::create('SEOHealthAnalysis', 'SEO Health Analysis', [
-                GoogleSearchPreview::create(
-                    'GoogleSearchPreview',
-                    'Search Preview',
-                    $this->getOwner(),
-                    $this->getRenderedHtmlDomParser()
-                ),
-                TextField::create('FocusKeyword', 'Set focus keyword'),
-                HealthAnalysisField::create('ContentAnalysis', 'Content Analysis', $this->getOwner()),
-            ])
-        ], 'Metadata');
+            $fields->addFieldsToTab('Root.Main', [
+                ToggleCompositeField::create('SEOHealthAnalysis', 'SEO Health Analysis', [
+                    GoogleSearchPreview::create(
+                        'GoogleSearchPreview',
+                        'Search Preview',
+                        $this->getOwner(),
+                        $this->getRenderedHtmlDomParser()
+                    ),
+                    TextField::create('FocusKeyword', 'Set focus keyword'),
+                    HealthAnalysisField::create('ContentAnalysis', 'Content Analysis', $this->getOwner()),
+                ])
+            ], 'Metadata');
+        }
     }
 }
--- src/Extensions/PageSeoExtension.php
+++ src/Extensions/PageSeoExtension.php
@@ -102,52 +102,53 @@ class PageSeoExtension extends DataExtension
      */
     public function updateCMSFields(FieldList $fields)
     {
-        parent::updateCMSFields($fields);
-        $suppressMessaging = false;
-        if (Controller::curr() instanceof HistoryViewerController) { // avoid cluttering the history comparison UI
-            $suppressMessaging = true;
-        }
+        if (!$fields->dataFieldByName('FacebookPageType')) {
+                $suppressMessaging = false;
+            if (Controller::curr() instanceof HistoryViewerController) { // avoid cluttering the history comparison UI
+                $suppressMessaging = true;
+            }
 
-        $fields->addFieldsToTab('Root.Main', [
-            ToggleCompositeField::create('FacebookSeoComposite', 'Facebook SEO', [
-                DropdownField::create('FacebookPageType', 'Type', FacebookMetaGenerator::getValidTypes()),
-                TextField::create('FacebookPageTitle', 'Title')
-                    ->setAttribute('placeholder', $this->getOwner()->Title)
-                    ->setRightTitle($suppressMessaging ? '' : 'If blank, inherits default page title')
-                    ->setTargetLength(45, 25, 70),
-                UploadField::create('FacebookPageImage', 'Image')
-                    ->setRightTitle($suppressMessaging
-                        ? ''
-                        : 'Facebook recommends images to be 1200 x 630 pixels. ' .
-                        'If no image is provided, Facebook will choose the first image that appears on the page, ' .
-                        'which usually has bad results')
-                    ->setFolderName('seo'),
-                TextareaField::create('FacebookPageDescription', 'Description')
-                    ->setAttribute('placeholder', $this->getOwner()->MetaDescription ?:
-                     $this->getOwner()->dbObject('Content')->LimitCharacters(297))
-                    ->setRightTitle($suppressMessaging
-                        ? ''
-                        : 'If blank, inherits meta description if it exists ' .
-                        'or gets the first 297 characters from content')
-                    ->setTargetLength(200, 160, 320),
-            ]),
-            ToggleCompositeField::create('TwitterSeoComposite', 'Twitter SEO', [
-                TextField::create('TwitterPageTitle', 'Title')
-                    ->setAttribute('placeholder', $this->getOwner()->Title)
-                    ->setRightTitle($suppressMessaging ? '' : 'If blank, inherits default page title')
-                    ->setTargetLength(45, 25, 70),
-                UploadField::create('TwitterPageImage', 'Image')
-                    ->setRightTitle($suppressMessaging ? '' : 'Must be at least 280x150 pixels')
-                    ->setFolderName('seo'),
-                TextareaField::create('TwitterPageDescription', 'Description')
-                    ->setAttribute('placeholder', $this->getOwner()->MetaDescription ?:
+            $fields->addFieldsToTab('Root.Main', [
+                ToggleCompositeField::create('FacebookSeoComposite', 'Facebook SEO', [
+                    DropdownField::create('FacebookPageType', 'Type', FacebookMetaGenerator::getValidTypes()),
+                    TextField::create('FacebookPageTitle', 'Title')
+                        ->setAttribute('placeholder', $this->getOwner()->Title)
+                        ->setRightTitle($suppressMessaging ? '' : 'If blank, inherits default page title')
+                        ->setTargetLength(45, 25, 70),
+                    UploadField::create('FacebookPageImage', 'Image')
+                        ->setRightTitle($suppressMessaging
+                            ? ''
+                            : 'Facebook recommends images to be 1200 x 630 pixels. ' .
+                            'If no image is provided, Facebook will choose the first image that appears on the page, ' .
+                            'which usually has bad results')
+                        ->setFolderName('seo'),
+                    TextareaField::create('FacebookPageDescription', 'Description')
+                        ->setAttribute('placeholder', $this->getOwner()->MetaDescription ?:
                         $this->getOwner()->dbObject('Content')->LimitCharacters(297))
-                    ->setRightTitle($suppressMessaging
-                        ? ''
-                        : 'If blank, inherits meta description if it exists ' .
+                        ->setRightTitle($suppressMessaging
+                            ? ''
+                            : 'If blank, inherits meta description if it exists ' .
                             'or gets the first 297 characters from content')
-                    ->setTargetLength(200, 160, 320),
-            ])
-        ], 'Metadata');
+                        ->setTargetLength(200, 160, 320),
+                ]),
+                ToggleCompositeField::create('TwitterSeoComposite', 'Twitter SEO', [
+                    TextField::create('TwitterPageTitle', 'Title')
+                        ->setAttribute('placeholder', $this->getOwner()->Title)
+                        ->setRightTitle($suppressMessaging ? '' : 'If blank, inherits default page title')
+                        ->setTargetLength(45, 25, 70),
+                    UploadField::create('TwitterPageImage', 'Image')
+                        ->setRightTitle($suppressMessaging ? '' : 'Must be at least 280x150 pixels')
+                        ->setFolderName('seo'),
+                    TextareaField::create('TwitterPageDescription', 'Description')
+                        ->setAttribute('placeholder', $this->getOwner()->MetaDescription ?:
+                            $this->getOwner()->dbObject('Content')->LimitCharacters(297))
+                        ->setRightTitle($suppressMessaging
+                            ? ''
+                            : 'If blank, inherits meta description if it exists ' .
+                                'or gets the first 297 characters from content')
+                        ->setTargetLength(200, 160, 320),
+                ])
+            ], 'Metadata');
+        }
     }
 }
