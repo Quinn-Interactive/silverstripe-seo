d8ff16613d5c718f3ad4adabc4385331fc74ea60
Fred Condo <fcondo@quinn.com>
2024-08-20 16:36:09 -0700

    Install & configure rector

---
+++ rector.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withPhpSets(php80: true)
+    ->withImportNames(importShortClasses: false)
+    ->withTypeCoverageLevel(0);
